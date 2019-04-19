5b4f775671ced521b455942e44bf8280a1bf7e4b
TonyPnode <sgtp@me.com>
2019-04-19 17:02:38 -0400

    WIP: Add bills list
    Signed-off-by: TonyPnode <sgtp@me.com>

--- money_bags/settings.py
+++ money_bags/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lusqmb1q#(&v(fqfxez6_fsuf94^2^bxfuot7uwoq3tfge-+vh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
